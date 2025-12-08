c8185b40ff92f698c2de96f009ba43bcb05c17e5
muskan <muskantuteja714@gmail.com>
2025-12-08 14:29:24 +0530

    08/12/25

---
+++ JS/Day2/script.js
@@ -0,0 +1,31 @@
+// data type
+// typeof
+let no = 1.3456;
+console.log(no);
+console.log(typeof no);
+
+// string
+let str = "mahi";
+console.log(str);
+console.log(typeof str);
+// color full comments
+
+//& null
+//  symbol : it is used to uniquely specifing the variables.
+let syb1 = Symbol(12345);
+let sub2 = Symbol(34590);
+console.log(typeof sub1);
+console.log(syb1 == sub2);
+//null & undefined
+// null: it is assisgned as a value,which represent the absences of the value
+let data = null
+//undefined: when the variable is declared, nut value is not assignrd is the variables
+
+// primitive/mutable :which can be changed
+//function typepf function
+// array typeof object
+//object typeof object
+
+// output methods in js:
+
+
