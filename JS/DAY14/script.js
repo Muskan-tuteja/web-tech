5a1c45f5c4184e837f73af76f0bf7d63b80e6b51
muskan <muskantuteja714@gmail.com>
2026-01-02 11:27:28 +0530

    02/01/26

--- JS/DAY14/script.js
+++ JS/DAY14/script.js
@@ -1,36 +1,52 @@
-//! Literal's Way 
+//! Literal's way
 
 // let obj ={
-//   id : 1,
-//   eName : "muskan"
+//     id : 1,
+//     eName : "abc",
+//     sal : 12345 
 // }
-//! Constructor's way
-// let obj = new Object({id:1,eName:"adb",sal:899888})
-// // console.log(obj)
-// let newObj = Object.create(obj)
-// console.log(newObj)
-// console.log(newObj.eName)
-// console.log(newObj.sal)
 
-// //? Create 
-// newObj.newPop="xyz"
-// console.log(newObj)
+//! Consructor's way
 
-//? prototype : it is a object. In js every has an internal link to anthor object which is called it's prototype.
+let obj = new Object({id : 1, eName : "abc", sal : 12345})
+// console.log(obj)
 
-console.log(String)//captial string is constructors
-console.log( typeof(String.prototype))
-console.log( typeof(Array.prototype))
-console.log( typeof(Object.prototype))
+//! Object.create()
+ let newObj = Object.create(obj)
+ console.log(newObj)
+ console.log(newObj.eName)
+ console.log(newObj.sal)
 
-let arr = [1,2,3,45]
-console.log(Array.prototype)
-console.log(arr.__proto__)// array method
-console.log(arr.__proto__.__proto__)// object method
-console.log(window.__proto__.__proto__)
+ newObj.newProp = "xyz"
+ console.log(newObj)
+
+// console.log(String)
+// console.log(String.prototype)
+// console.log(typeof(String.prototype))
+
+// console.log(Array)
+// console.log(Array.prototype)
+
+ //! prototype = It is a object in JS every object has an internal link to another object which is called it's prototype.
+
+//! Prototypal Inheritance : 
+// it is a feature in a JS where objects inheriet properties and methods from other object forming a prototype chain this is known as PI.
+// console.log(window)
+
+// let arr = [1,2,3,4,,5]
+// console.log(Array.prototype)        //constructor k liye
+// console.log(_proto_)              //instance(just like copy)
+// console.log(arr._proto.proto_)
+
+// console.log(Array.prototype == arr._proto_)
+
+// console.log(window._proto.proto.proto.proto_)
+
+// let obj = {
+//    id : 1,
+//    eName : "abc" 
+// }
 
 
-
-//? Prota inhertances : it is features is js . where as object inhertani propert and method from anthor object form a proporty change is 
-
-//? prototype vs __proto: proto type is canscatrion which is used to  stor share method  while __proto__ is as iterenal of an object that point its concatrion proportype 
+//! prototype V/S _proto_
+// prototype is a property of a constructor function , which is used to store shared method while _proto_ is a internal refrence of an object that points to it's consuctor prototype.
