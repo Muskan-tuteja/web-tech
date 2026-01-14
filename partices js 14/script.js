7352ade5560fab447cb3a7c17a5c125de24c7b02
muskan <muskantuteja714@gmail.com>
2026-01-14 22:24:14 +0530

    14/01/26

---
+++ partices js 14/script.js
@@ -0,0 +1,27 @@
+// let str = "Muskan"
+// console.log(str.repeat(2))
+// console.log(str.repeat(0))
+// console.log(str.repeat(2))
+
+let str = "My name is muskan, My age is 23, My father is man"
+console.log(str.replace("My","I"));
+console.log(str.replaceAll("My","I"));
+console.log(str.indexOf("My"));
+console.log(str.indexOf("M"));
+console.log(str.indexOf("name"));
+console.log(str.indexOf("n"));
+console.log(str.indexOf("is"));
+console.log(str.indexOf("f"));
+console.log(str.indexOf("y"));
+console.log(str.indexOf("g"));
+console.log(str.indexOf("d"));
+console.log(str.indexOf("n"));
+console.log(str.lastIndexOf("n"));
+console.log(str.includes("my"));
+console.log(str.match("My"));
+console.log(str.match("dog"));
+console.log(str.match("M"));
+
+
+
+
