5201d1eef634fabe70cf1546cc3354fa30ca92b7
muskan <muskantuteja714@gmail.com>
2026-01-28 13:44:54 +0530

    24/01/26

---
+++ JS/day25/script.js
@@ -0,0 +1,25 @@
+//! API :
+//! APPLICATION PROGRAMMING INTERFACE
+//it is like bridge whic is used connecting your front-end and back-end.
+// api is a medium which interact with client and serevr.
+//api takes request from the client and gives it to the server and text the information from the server and give it to the client.
+
+//!json
+//stands for java script object notation . 
+// it is a light weight data exchange format b/w client and server
+//json object is very similar to the java script object , where the key must b double quotted string in case of json object.
+
+//!fetch 
+// it is a method which is used for fetching the data from the api/server and ir return a promise.
+
+// settimeout is a method which is used for making our js code async.it is accept two arguments---
+// 1.callback function
+// 2. time in mili seconds
+
+// console.log("Task 1 Complete")
+// setTimeout(() => {
+//     console.log("Task 2 complete")
+    
+// }, 2000);
+// console.log("Task 3 Complete")
+// console.log("Task 4 Complete")
