3b7bd1a7739879bab659856c865cddbdb8297704
muskan <muskantuteja714@gmail.com>
2026-02-04 15:15:00 +0530

    04/02/26

--- Final Projects/Login.js
+++ Final Projects/Login.js
@@ -1,10 +1,22 @@
-let email = document.querySelector(".email")
-let password = document.querySelector(".password")
-
 // console.log(email,password)
 
-let form = document.querySelector("form")
+let form = document.querySelector("form");
 
-form.addEventListener("submit",()=>{
-  let users = JSON.parse(localStorage.getItem("users")) || []
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = document.querySelector(".email").value.trim()
+  let password = document.querySelector(".password").value.trim()
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+   let userExits = users.some((user)=>{
+    return user.email === email && user.password === password
+  })
+  if(userExits){
+    alert("Logeed Successfull")
+    window.location.href= "./Home.html"
+    return
+  }else{
+    alert("Username and password are not matched")
+    return;
+  }
+});
