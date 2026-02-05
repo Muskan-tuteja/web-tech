de3b4b82618314ac86c9fe27fbf41fc89e7eb56c
muskan <muskantuteja714@gmail.com>
2026-02-05 15:51:19 +0530

    05/02/26

--- Final Projects/Home.js
+++ Final Projects/Home.js
@@ -5,11 +5,20 @@ async function apiFetch() {
   console.log(user)
 
   //find the main container where we want to put our cards
-  let mainDiv = document.getElementById("#mainDiv")
+  let mainDiv = document.getElementById("mainDiv")
   console.log(mainDiv)
-
+// //loop through each user and create a card
   user.forEach(user => {
     console.log(user)
+// create a card container
+    let card = document.createElement("div")
+    card.classList.add("cardDiv")
+//put data(Content) inside the card
+    card.innerHTML = `<h3> Product No: ${user.id}</h3>
+  <img src="${user.images}" alt="${user.title}">
+  <button class="viewButton">Add to card</button>`
+//add the card to the page
+  mainDiv.appendChild(card)
     
   });
   
