3b6a60b612649df8859ef3e0cacdb5c1682309f9
muskan <muskantuteja714@gmail.com>
2026-02-10 00:53:36 +0530

    09/02/26

---
+++ Final Pratices in js/script.js
@@ -0,0 +1,32 @@
+// var a = 10
+// let b = 20
+// const c = 40
+// console.log(a);
+// console.log(b)
+// console.log(c)
+// {
+// var a = 10
+// let b = 20
+// const c = 40
+
+// // console.log(a);
+// // console.log(b)
+// // console.log(c)
+
+// }
+// console.log(a);
+// console.log(b)
+// console.log(c)
+
+function abc (){
+  var a = 10
+let b = 20
+const c = 40
+console.log(a);
+console.log(b)
+console.log(c)
+}
+abc()
+// console.log(a);
+// console.log(b)
+// console.log(c)
