14505c31869e7f2b96eca6918f101c945b2ff97a
muskan <muskantuteja714@gmail.com>
2026-02-10 21:00:22 +0530

    10/02/26

--- Final Pratices in js/script.js
+++ Final Pratices in js/script.js
@@ -31,10 +31,53 @@
 // console.log(b)
 // console.log(c)
 
+// console.log(a)
+// console.log(b)
+// console.log(c)
+//  var a = 10
+//  let b = 20
+//  const c = 10
 
-console.log(a)
-console.log(b)
-console.log(c)
- var a = 10
- let b = 20
- const c = 10
+// let age = 20
+// if (age>=18) {
+//   console.log("you are the eligible age ");
+
+// }
+
+// let marks = 1;
+// if (marks >= 0) {
+//   console.log("pass");
+// } else {
+//   console.log("Fail");
+// }
+
+// let grade = 90;
+// if (grade >= 95) {
+//   console.log("A grade");
+// } else if (grade >= 80) {
+//   console.log("B Grade");
+// } else if (grade >= 67) {
+//   console.log("c grader");
+// } else {
+//   console.log("fail");
+// }
+
+let day = 2;
+switch (day) {
+  case 1:
+    console.log("Monday");
+
+    break;
+  case 2:
+    console.log("Tuesday");
+
+    break;
+  case 3:
+    console.log("webnesday");
+
+    break;
+
+  default:
+    console.log("Invalida day");
+    
+}
